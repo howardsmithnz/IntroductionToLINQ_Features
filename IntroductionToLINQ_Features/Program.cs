9e446ed3d1b14ebba6134ea9fbbfbd8dea153877
Howard Smith <howard.smith@finnz.com>
2019-12-16 14:33:25 +1300

    Add project files.

---
+++ IntroductionToLINQ_Features/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToLINQ_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
